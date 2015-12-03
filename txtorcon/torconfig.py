557c062bdf44036b8fc4b41b29d14f9b08efa495
meejah <meejah@meejah.ca>
2015-12-02 23:19:58 -0700

    incorrect rebase conflict-resolution

--- txtorcon/torconfig.py
+++ txtorcon/torconfig.py
@@ -917,60 +917,6 @@ class EphemeralHiddenService(object):
         yield uploaded
         yield protocol.remove_event_listener('HS_DESC', hs_desc)
 
-        log.msg('Created hidden-service at', self.hostname)
-
-        # Now we want to wait for the descriptor uploads. This doesn't
-        # quite work, as the UPLOADED events always say "UNKNOWN" for
-        # the HSAddress so we can't correlate it to *this* onion for
-        # sure :/ "yet", though. Yawning says on IRC this is coming.
-
-        # XXX Hmm, still UPLOADED always says UNKNOWN, but the UPLOAD
-        # events do say the address -- so we save all those, and
-        # correlate to the target nodes. Not sure if this will really
-        # even work, but better than nothing.
-
-        uploaded = defer.Deferred()
-        attempted_uploads = set()
-        confirmed_uploads = set()
-        failed_uploads = set()
-
-        def hs_desc(evt):
-            """
-            From control-spec:
-            "650" SP "HS_DESC" SP Action SP HSAddress SP AuthType SP HsDir
-            [SP DescriptorID] [SP "REASON=" Reason] [SP "REPLICA=" Replica]
-            """
-
-            args = evt.split()
-            subtype = args[0]
-            if subtype == 'UPLOAD':
-                if args[1] == self.hostname[:-6]:
-                    attempted_uploads.add(args[3])
-
-            elif subtype == 'UPLOADED':
-                # we only need ONE successful upload to happen for the
-                # HS to be reachable.
-                addr = args[1]
-                if args[3] in attempted_uploads:
-                    confirmed_uploads.add(args[3])
-                    log.msg("Uploaded '{}' to '{}'".format(self.hostname, args[3]))
-                    uploaded.callback(self)
-
-            elif subtype == 'FAILED':
-                if args[1] == self.hostname[:-6]:
-                    failed_uploads.add(args[3])
-                    if failed_uploads == attempted_uploads:
-                        msg = "Failed to upload '{}' to: {}".format(
-                            self.hostname,
-                            ', '.join(failed_uploads),
-                        )
-                        uploaded.errback(RuntimeError(msg))
-
-        log.msg("Created '{}', waiting for descriptor uploads.".format(self.hostname))
-        yield protocol.add_event_listener('HS_DESC', hs_desc)
-        yield uploaded
-        yield protocol.remove_event_listener('HS_DESC', hs_desc)
-
     @defer.inlineCallbacks
     def remove_from_tor(self, protocol):
         '''
